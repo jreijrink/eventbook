abf3b32424ae5d0a5a06cf351f6da980a73dc876
kirsttje <kirsten@solcon.nl>
2015-10-06 16:10:00 +0200

    forgot to commit this for the new spellchecker word

--- querying/views.py
+++ querying/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from querying.spelchecker import checkSpelling
+from querying.spellchecker import checkSpelling
 from querying.decomposition import decompose
 from querying.searchhistory import applySearchHistory
 from querying.indexer import retrieveFromIndex
