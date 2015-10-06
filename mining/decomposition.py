df3d66022c107a8d52a549440b0bb17478077b6c
RobbertRaats <r.a.s.raats@student.tue.nl>
2015-10-06 17:18:29 +0200

    Added lemmatizer (stemming which result in real words)

--- mining/decomposition.py
+++ mining/decomposition.py
@@ -1,12 +1,13 @@
 from mining.contractions import expandContractions
 from collections import OrderedDict
+from nltk.stem.wordnet import WordNetLemmatizer
 import re, string
 
 def decomposeDocument(text):
     print("DECOMPOSITION");
     print(text);
 
-    # Replace all uppercase letters to lowercase letters
+    # Case-folding
     text = text.lower();
     print(text);
     
@@ -23,10 +24,16 @@ def decomposeDocument(text):
     text = ' '.join(OrderedDict((word,word) for word in text.split()).keys());
     print(text);
     
-    # Remove redundant words (just add words to the list you think also have to be removed)
-    redundantWords = ['the','this','that','those','these','to','as','there','has','and','or','is','not','a','an','of','but','in','by','on','are','it','if'];
+    # Remove stop words (just add words to the list you think also have to be removed)
+    stopWords = ['the','this','that','those','these','to','as','there','has','and','or','is','not','a','an','of','but','in','by','on','are','it','if'];
     words = text.split();
-    text = ' '.join([i for i in words if i not in redundantWords]);
+    text = ' '.join([i for i in words if i not in stopWords]);
+    print(text);
+    
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer();
+    words = text.split();
+    text = ' '.join([lemmatizer.lemmatize(i) for i in words]);
     print(text);
     
     print("END DECOMPOSITION");
--- mining/scraper.py
+++ mining/scraper.py
@@ -11,7 +11,7 @@ def findDocuments():
     document = NewDocument()
 
     document.title = "Festival Title"
-    document.description = "This isn't a Festival title, but it's a Festival description...!"
+    document.description = "This isn't a Festival title, but it's a Festival description...! cars"
     document.date = "01-01-2016"
     document.location = "New York"
     
