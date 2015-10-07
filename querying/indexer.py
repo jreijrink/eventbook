1c413ad6ea4b117774bfd6274d6e45657788c82b
Stefanie Lyu <185476041@qq.com>
2015-10-07 22:50:31 +0200

    idf

--- querying/indexer.py
+++ querying/indexer.py
@@ -4,63 +4,61 @@ from itertools import chain
  
 from querying.caching import retrieveFromCache 
 from querying.caching import saveToCache 
-
  
+import math 
 import logging 
-import math
 logger = logging.getLogger("eventbook") 
  
  
 def retrieveFromIndex(query): 
     cache = retrieveFromCache(query) 
-    
+   
     if cache is not None: 
         logger.debug('Cache contained results for this query!') 
         return cache 
     else: 
         logger.debug('No results in cache') 
-         
+        
         docnumber = len(Document.objects.all())
-        print(docnumber)
-
+        print(docnumber) 
+        
         Dict={}
         for document in Document.objects.all():#something wrong
             Dict[document]=0.0
             print(Dict[document])
-           
+        
+        documents = set() 
+        resultDocument = Document.objects.none() 
+         
         words = query.split() 
          
         for word in words: 
             tokens = Token.objects.filter(name__iexact=word) 
             #tokens = Token.objects.filter(name__contains=word) 
             for token in tokens: 
-                resultDocument=set()
+                resultDocument = set()
                 titleResults = token.title_tokens.all() 
                 dateResults = token.date_tokens.all() 
                 locationResults = token.location_tokens.all() 
                 genreResults = token.genres_tokens.all() 
                 artistResults = token.artist_tokens.all() 
-                tagResults = token.tag_tokens.all()
+                tagResults = token.tag_tokens.all() 
                  
                 resultDocument = chain(resultDocument, titleResults, dateResults, locationResults, genreResults, artistResults, tagResults) 
 
-                documents = set()
-
+ 
                 for document in resultDocument: 
                     if document not in documents: 
                         documents.add(document) 
-
-                
+                        
                 df = len(documents)
-                print(df)
                 idf=math.log((docnumber/df),2)
-                print(idf)
-                
+
                 for document in documents:
                     Dict[document] +=  idf
-        # TF.IDF here 
-        # 
 
+        for document in Document.objects.all():
+            print(Dict[document])
  
 
  
