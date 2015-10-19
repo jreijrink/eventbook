3be36d4bda45911f02366b6b3747f72879ef599a
jreijrink <j.reijrink@student.tue.nl>
2015-10-19 11:36:32 +0200

    disabled removal of tokens with number and symbols. Also made for loops more readable

--- common/tokenizer.py
+++ common/tokenizer.py
@@ -3,25 +3,20 @@ import nltk
 import re
 
 def getTokensFromText(text):
+    tokens = list()
+    
     # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
-    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-    filtered_tokens = []
-    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
-    for token in tokens:
-        if re.search('[a-zA-Z]', token):
-            filtered_tokens.append(token)
-    return filtered_tokens
-
-
-#def getTokensFromText(text):
- #   tokens = list()
-  #  text_items = text.split(" ")
-   # for text_item in text_items:
-    #    tokens.append(text_item)
-    #return tokens
+    for sentence in nltk.sent_tokenize(text):
+         for word in nltk.word_tokenize(sentence):
+            token = word.lower()
+            # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+            #if re.search('[a-zA-Z]', token):
+            tokens.append(token)
+            
+    return tokens
 
 def getTokensFromList(textList):
     tokens = list()
-    for text in textList:
-        tokens.append(text)
+    for text in textList:   
+        tokens.extend(getTokensFromText(text))
     return tokens
