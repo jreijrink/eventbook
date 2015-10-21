0cb69f1cab3640b11cb64ce2c14a90165383fea9
kirsttje <kirsten@solcon.nl>
2015-10-21 14:37:30 +0200

    To use this, you have to do the following steps:
    1. Go to .cmd, the command screen.
    Type: python
    Enter
    
    Type: import pip
    Enter
    
    Type: print(pip.pep425tags.get_supported())
    Enter
    
    Look at what is written behind  win: either 32 or 64
    
    2
    Go to: http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
    Download:
    
    If you have win32:
    scipy‑0.16.0‑cp34‑none‑win32.whl
    numpy‑1.10.1+mkl‑cp34‑none‑win32.whl
    
    If you have win64:
    scipy‑0.16.0‑cp34‑none‑win64.whl
    numpy‑1.10.1+mkl‑cp34‑none‑win64.whl
    
    3. Move both downloaded files to C:/
    
    4. Type in cmd:
    
    If you have 32:
    pip install C:/scipy‑0.16.0‑cp34‑none‑win32.whl
    enter
    
    pip install C:numpy‑1.10.1+mkl‑cp34‑none‑win32.whl
    enter
    
    If you have 64:
    pip install C:/scipy‑0.16.0‑cp34‑none‑win64.whl
    enter
    
    pip install C:numpy‑1.10.1+mkl‑cp34‑none‑win64.whl
    enter

--- mining/clustering.py
+++ mining/clustering.py
@@ -1,2 +1,19 @@
-def clusterDocument(document):
-    return document
+from sklearn.cluster import KMeans
+
+def initialClustering(tfidf_matrix):
+    num_clusters = 5 #chang
+
+    km = KMeans(n_clusters=num_clusters)
+
+    km.fit(tfidf_matrix)
+
+    clusters = km.labels_.tolist()
+
+    return km
+
+def clusterDocument(km, document):
+
+    return km.predict(document)
+
+# def clusterDocument(document):
+#    return document
