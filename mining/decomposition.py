1a0623fc1b96cdf9a2a4e2fe4cb315b9f5fa3588
RobbertRaats <r.a.s.raats@student.tue.nl>
2015-10-26 12:11:17 +0100

    Remove printed text from console

--- mining/decomposition.py
+++ mining/decomposition.py
@@ -5,38 +5,27 @@ import re, string
 
 def decompose(text):
     if text:
-        print("DECOMPOSITION");
-        print(text);
-    
         # Case-folding
         text = text.lower();
-        print(text);
         
         # Expand all contractions like "isn't" to "is not"
         text = expandContractions(text);
-        print(text);
         
         # Remove punctuation
         regex = re.compile('[%s]' % re.escape(string.punctuation))
         text = regex.sub('', text)
-        print(text);
         
         # Remove stop words (just add words to the list you think also have to be removed)
         stopWords = ['the','this','that','those','these','to','as','there','has','and','or',
                      'is','not','a','an','of','but','in','by','on','are','it','if'];
         words = text.split();
         text = ' '.join([i for i in words if i not in stopWords]);
-        print(text);
         
         # Lemmatization
         lemmatizer = WordNetLemmatizer();
         words = text.split();
         text = ' '.join([lemmatizer.lemmatize(i) for i in words]);
-        print(text);
         
         # Remove duplicate words
         text = ' '.join(OrderedDict((word,word) for word in text.split()).keys());
-        print(text);
-        
-        print("END DECOMPOSITION");
     return text
--- querying/synonyms.py
+++ querying/synonyms.py
@@ -12,15 +12,11 @@ def lemmalist(str):
 
 # Returns a string containing all synonyms for the words in an input string.
 def returnSynonyms(text):
-    print("ADDING SYNONYMS");
     subText = text;
     words = subText.split();
     for word in words:
         if not lemmalist(word):
             subText = subText;
         else: subText = ' '.join([i for i in lemmalist(word)]);
-        print(subText);
         text = text + ' ' + subText;
-    print(text);
-    print("SYNONYMS ADDED");
     return text
